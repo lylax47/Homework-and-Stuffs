09e1bc38c918746828044f6cbe3c03073f853dd9
Lylax47 <jjlyell@gmail.com>
2016-02-24 20:16:12 +0300

    Crawler

---
+++ Краулер/Olemka.py
@@ -0,0 +1,74 @@
+def csv_write(path, author, header, created, topic, source, publ_year, doc_counter):
+    fir_row = ['path', 'author', 'sex', 'birthday', 'header', 'created', 'sphere', 'genre_fi',  #all headers
+    'type', 'topic', 'chronotop', 'style', 'audience_age', 'audience_level', 'audience_size',
+    'source', 'publication', 'publisher', 'publ_year', 'medium', 'country', 'region', 'language']
+    info_row = [path, author, '', '', header, created, 'публистика', '', '', topic, '', 'нейтральный',  #info
+    'н-возраст', 'н-уровень', 'районная', source, 'Олёмка', '', publ_year, 'газета', 'Россия',
+    'Якутия', 'ru']
+    with open('info.csv', 'a', newline = '', encoding = 'utf-8') as csv_f:
+        csv_file = csv.writer(csv_f, delimiter = ',', quotechar = '|', 
+            quoting = csv.QUOTE_MINIMAL)
+        if doc_counter == 1:             #so that csv is only appended with first row once
+            csv_file.writerow(fir_row)
+        csv_file.writerow(info_row)
+
+def find_author(tree):
+    au_name_list = tree.xpath('.//em/strong/text()')
+    for text in au_name_list:
+        m = re.match('[А-Я]\.\s?([А-ЯЁ]|[а-яё])+', text) #
+        if m:
+            name = m.group()
+            break
+    if au_name_list == [] or m == None :
+        name = 'Noname'
+    return name
+
+
+from lxml import html
+import requests, os.path, sys, re, time, csv
+try:                        #remove the csv file if it exists already, so it doesn't keep appending
+    os.remove('info.csv')
+except OSError:
+    pass
+href_skip = [] ## used for stiring visited sites
+doc_counter = 1 ## used for creating ordinal file names
+main_pg = requests.get("http://gazetaolekma.ru") ## input site here
+tree = html.fromstring(main_pg.content)
+hrefs = tree.xpath('//a[starts-with(@href, "http://gazetaolekma.ru") or starts-with(@href,"https://gazetaolekma.ru") or starts-with(@href,"ftp://gazetaolekma.ru")]/@href')  ## To avoid non-absolute hrefs
+for href in hrefs:
+    time.sleep(3) ##to be nice, and so I stop getting banned
+    link_pg = requests.get(href)
+    tree2 = html.fromstring(link_pg.content)
+    if tree2.find('.//p') and tree2.find('.//*[@class="e-title"]') is not None and href not in href_skip:  ## If no text content (not an article), or site already visited, skips writing to text file
+        doc_title = tree2.xpath('//*[@class="e-title"]/text()')  ## selects title of text from each link for file name
+        clean_title = re.sub(r'(\?|\\|\?|\%|\*|\:|\|\||"|<|>)',r'',''.join(doc_title)) ##remove invalid chars
+        theme = tree2.xpath('//span[@class="news-details-cat"]/a/text()')
+        if theme == []:
+            theme = 'Notopic'
+        else:
+            theme = theme[0]
+        doc_content = str(tree2.xpath('.//p/text() | .//p/strong/text() | .//p/em/text() | .//p/.//span/text()')) ## extracts only text
+        clean_doc_con = re.sub(r'((\', \')|\'|\\xa0|\\n|\[|\])',r'',''.join(doc_content))        ## remove unnecessary symbols
+        au_name = find_author(tree2)
+        date = tree2.xpath('//a[@class = "dateBar"]/text()')
+        year = date[0]
+        month = date[1]
+        day = date[2]
+        clean_date = "{0}.{1}.{2}".format(day, month, year)
+        html_dir = os.path.join(os.getcwd(),"html\\{0}\\{1}".format(year, month))
+        no_marks_dir = os.path.join(os.getcwd(),"no_marks\\{0}\\{1}".format(year, month))
+        if not os.path.exists(html_dir):
+            os.makedirs(html_dir)
+        if not os.path.exists(no_marks_dir):
+            os.makedirs(no_marks_dir)
+        with open("{0}/{1}.txt".format(html_dir, str(doc_counter)), "w", encoding = 'utf-8') as html_doc,\
+        open("{0}/{1}.txt".format(no_marks_dir, str(doc_counter)), "w", encoding = 'utf-8') as no_marks_doc:
+            html_doc.write(link_pg.text)
+            no_marks_doc.write("@ {0}\n@ {1}\n@ {2}\n@ {3}\n@ {4}\n {5}".format(au_name, clean_title, clean_date, theme, href, clean_doc_con))
+        csv_write(no_marks_dir, au_name, clean_title, clean_date, theme, href, year, doc_counter)
+        doc_counter = doc_counter + 1
+        href_skip.append(href) #appends list with already completed link
+    hrefs2 = tree2.xpath('//a[starts-with(@href, "http://gazetaolekma.ru") or starts-with(@href,"https://gazetaolekma.ru") or starts-with(@href,"ftp://gazetaolekma.ru")]/@href')
+    for href2 in hrefs2:
+        if href2 not in hrefs:
+            hrefs.append(href2)
---
+++ Краулер/Stemmer.py
@@ -0,0 +1,28 @@
+import os, json, dicttoxml
+from pymystem3 import Mystem
+
+m = Mystem()
+top = 'C:\\Users\\John\\Desktop\\py_files\\питон\\korpus\\no_marks'
+for root, dirs, files in os.walk(top):
+    for name in files:
+        loc = os.path.join(root, name)
+        loc_list = loc.split('\\')
+        new_root = loc.replace('\\no_marks\\{0}\\{1}\\{2}'.format(loc_list[8], loc_list[9], loc_list[10]), '')
+        dir_marks = os.path.join(new_root + '\\marks\\{0}\\{1}'.format(loc_list[8], loc_list[9]))
+        dir_xml = os.path.join(new_root + '\\xml\\{0}\\{1}'.format(loc_list[8], loc_list[9]))
+        new_name = name.replace('.txt', '')
+        if not os.path.exists(dir_marks):
+            os.makedirs(dir_marks)
+        if not os.path.exists(dir_xml):
+            os.makedirs(dir_xml)
+        with open(loc, "r", encoding = 'utf-8') as doc:
+            text_doc = doc.read()
+            lines = doc.readlines()
+            info = json.dumps(m.analyze(text_doc), ensure_ascii = False)
+        with open("{0}\\{1}.json".format(dir_marks, new_name), 'w', encoding = 'utf-8') as doc_marks:
+            doc_marks.write(info)
+        xml = dicttoxml.dicttoxml(info).decode('utf-8')
+        with open("{0}\\{1}.xml".format(dir_xml, new_name), 'w', encoding = 'utf-8') as doc_xml:
+            doc_xml.write(xml)
+
+
