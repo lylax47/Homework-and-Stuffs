4a68712db58b0b0d9d79b1e75f7c3aa58f7261d8
lylax47 <john@john-K55VD.(none)>
2016-06-20 13:56:49 +0300

    upd

--- SQL/SQL.py
+++ SQL/SQL.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import MySQLdb
 import codecs
 import csv
@@ -64,13 +65,14 @@ def insert(table, db, vk_info, cursor):
     else:
         sex = 1    
     for row in vk_info[sex]:
-        args = (row[0], row[1], row[2], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+        args = (row[0], row[1], row[2], row[4].decode('utf8'), row[5].decode('utf8'), row[6].decode('utf8'), \
+            row[7].decode('utf8'), row[8].decode('utf8'), row[9].decode('utf8'), row[10].decode('utf8'))
         if sex == 0:
             comm = """INSERT INTO KOGMEN \
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+                VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"""
         else:
             comm = """INSERT INTO KOGWOM \
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+                VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"""
         cursor.execute(comm, args)
         db.commit()
 
