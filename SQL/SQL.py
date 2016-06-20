d9dc8eb0c9851720f0d737e38980dc1230fe75b9
lylax47 <john@john-K55VD.(none)>
2016-06-20 03:35:07 +0300

    upd

--- SQL/SQL.py
+++ SQL/SQL.py
@@ -6,22 +6,27 @@ import csv
 
 
 def imp_info():
-	men = []
-	women = []
-	with open.codecs('vk.csv', 'r', encoding = 'utf-8') as csvfile:
-		csvreader = csv.reader(csvfile, delimiter = ';', quotechar = '|')
-		for row in csvreader:
-			if row[3] == 1:
-				women.append(row)
-			else:
-				men.append(row)
-	return (men, women)
+    men = []
+    women = []
+    with codecs.open('vk.csv', 'r', encoding = 'utf-8') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ';', quotechar = '|')
+        for row in csvreader:
+            print row
+            try:
+                if row[3] == 1:
+                    women.append(row)
+                else:
+                    men.append(row)
+            except IndexError:
+                print row
+                break
+    return (men, women)
 
 
 
 def create(cursor):
-	make_men = """CREATE TABLE KOGMEN (
-		ID INT
+    make_men = """CREATE TABLE KOGMEN (
+        ID INT
         FIRST_NAME  CHAR(50) NOT NULL,
         LAST_NAME  CHAR(50),
         CITY CHAR(50),  
@@ -33,8 +38,8 @@ def create(cursor):
         RELIGION CHAR(50)
         LANGUAGES CHAR(50))"""
 
-	make_wom = """CREATE TABLE KOGWOM (
-		ID INT
+    make_wom = """CREATE TABLE KOGWOM (
+        ID INT
         FIRST_NAME  CHAR(50) NOT NULL,
         LAST_NAME  CHAR(50),
         CITY CHAR(50),  
@@ -51,17 +56,21 @@ def create(cursor):
 
 
 def insert(table, db, vk_info, cursor):
-	for row in vk_info[0]:
-		comm = "INSERT INTO '%s' (ID, FIRST_NAME, \
-		       LAST_NAME, CITY, BIRTHDAY, HOME_TOWN, \
-		       RELATION, UNIVRSITY, GRADUATION, RELIGION, LANGUAGES) \
-		       VALUES ('%d', %s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % \
-		       (table, row[0], row[1], row[2], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
-		try:
-   			cursor.execute(sql)
- 			db.commit()
-		except:
-   			db.rollback()
+    if table == 'KOGMEN':
+        sex = 0
+    else:
+        sex = 1
+    for row in vk_info[sex]:
+        comm = "INSERT INTO '%s' (ID, FIRST_NAME, \
+               LAST_NAME, CITY, BIRTHDAY, HOME_TOWN, \
+               RELATION, UNIVRSITY, GRADUATION, RELIGION, LANGUAGES) \
+               VALUES ('%d', %s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % \
+               (table, row[0], row[1], row[2], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+        try:
+            cursor.execute(sql)
+            db.commit()
+        except:
+            db.rollback()
 
          
 
